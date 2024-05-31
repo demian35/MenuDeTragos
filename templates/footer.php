19287258a7701265cd7c076ec1078c56ecb93140
Demian <53890358+demian35@users.noreply.github.com>
2024-05-30 19:52:56 -0600

    plantillas del proyecto

---
+++ templates/footer.php
@@ -0,0 +1,15 @@
+<!-- Main Footer -->
+
+</div>
+
+
+<!-- jQuery -->
+<script src="../vendor/jQuery/jquery.min.js"></script>
+<!-- AdminLTE -->
+<script src="../vendor/AdminLTE/adminlte3.2.0.min.js"></script>
+<!-- Bootstrap -->
+<script src="../vendor/Bootstrap/bootstrap5.2.3.bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--icono del titulo de la pagina-->
+    <!-- Estilos personalisados-->
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="../vendor/Bootstrap/bootstrap5.2.3.min.css">
+    <!-- AdminLTE -->
+    <link rel="stylesheet" href="../vendor/AdminLTE/adminlte3.2.0.min.css">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="../fontawesome/css/fontawesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
+</head>
+<body>
+<body class="hold-transition sidebar-mini">
+    <!-- Header -->
+    <header>
+        <div class="container-fluid">
+            <div class="row banner">
+                <div class="col-2 col-lg-1 col-md-1 col-sm-2 col-xs-2">
+                </div>
+                <div class="col-4 col-lg-5 col-md-5 col-sm-4 col-xs-4" style="color:#FFF;">
+                </div>
+            </div>
+            <div class="row bannerEventos">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 txtEventos">
+
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
