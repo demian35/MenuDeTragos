fe98ca6215c0f31b32e8365c34da5c4503136568
Demian <53890358+demian35@users.noreply.github.com>
2024-06-03 15:26:37 -0600

    agregando color purpura al nav del sidevar

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,7 +1,7 @@
 <div class="wrapper">
 
     <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light" id="navheader">
         <!-- Left navbar links -->
         <ul class="navbar-nav">
             <li class="nav-item">
